033541da29edbf2accd0c6a457818cb7ba9dad0e
VeraKulemina <verakulemina@gmail.com>
2022-11-13 20:10:39 -0700

    commit tree a17da9cf938717e8d1f1406f75ad6bf5be94691a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: { reviews: { include: :user } })  end
 
 end
